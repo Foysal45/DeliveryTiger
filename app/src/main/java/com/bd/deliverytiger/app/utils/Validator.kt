6aa5dc9f6e34209507e4bfa486b0e8a333070906
Salim <selim.cse.bd@gmail.com>
2019-09-24 16:19:04 +0600

    login activity and fragments added1222

---
+++ app/src/main/java/com/bd/deliverytiger/app/utils/Validator.kt
@@ -0,0 +1,36 @@
+package com.bd.deliverytiger.app.utils
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+object Validator {
+    val VALID_EMAIL_ADDRESS_REGEX: Pattern = Pattern.compile(
+        "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+        Pattern.CASE_INSENSITIVE
+    )
+    val VALID_MOBILE_NUMBER_REGEX: Pattern = Pattern.compile(
+        "^(01|০১)?[0-9০১২৩৪৫৬৭৮৯]{9}$",
+        Pattern.CASE_INSENSITIVE
+    )
+    val VALID_MOBILE_NUMBER_REGEXAgain: Pattern = Pattern.compile(
+        "(8801|৮৮০১)?[0-9০১২৩৪৫৬৭৮৯]{9}$",
+        Pattern.CASE_INSENSITIVE
+    )
+
+    fun isValidEmail(email: String?): Boolean {
+        val matcher: Matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email)
+        return matcher.find()
+    }
+
+    fun isValidMobileNumber(mobileNumber: String?): Boolean {
+        val matcher: Matcher =
+            VALID_MOBILE_NUMBER_REGEX.matcher(mobileNumber)
+        return matcher.find()
+    }
+
+    fun isValidMobileNumberAgain(mobileNumber1: String?): Boolean {
+        val matcher: Matcher =
+            VALID_MOBILE_NUMBER_REGEXAgain.matcher(mobileNumber1)
+        return matcher.find()
+    }
+}
