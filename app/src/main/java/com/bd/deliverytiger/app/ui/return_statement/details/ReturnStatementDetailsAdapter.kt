29aa66d74900baff9349a7bb9db12e324093ae19
Maruf <maruf.ajkerdeal@gmail.com>
2021-01-17 18:14:08 +0600

    # Return Statement Details Complete

---
+++ app/src/main/java/com/bd/deliverytiger/app/ui/return_statement/details/ReturnStatementDetailsAdapter.kt
@@ -0,0 +1,62 @@
+package com.bd.deliverytiger.app.ui.return_statement.details
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bd.deliverytiger.app.api.model.return_statement.ReturnOrder
+import com.bd.deliverytiger.app.databinding.ItemViewDashboardReturnBinding
+
+class ReturnStatementDetailsAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val dataList: MutableList<ReturnOrder> = mutableListOf()
+    var onItemClicked: ((model: ReturnOrder) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding: ItemViewDashboardReturnBinding = ItemViewDashboardReturnBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewModel(binding)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ViewModel) {
+            val model = dataList[position]
+            val binding = holder.binding
+//
+            binding.orderId.text = model.id.toString()
+            binding.reference.text = model.collectionName
+            binding.customerName.text = model.customerName
+            binding.customerPhone.text = model.mobile
+            binding.status.text = model.comment
+
+//            Glide.with(binding.productImage).load(model.productImageLink).into(binding.productImage)
+//            binding.title.text = model.dealTitle
+//            binding.couponId.text = "কুপন কোড: ${DigitConverter.toBanglaDigit(model.couponId)}"
+//            binding.dealId.text = "ডিল কোড: ${DigitConverter.toBanglaDigit(model.dealId)}"
+        }
+    }
+
+    override fun getItemCount(): Int = dataList.size
+
+    inner class ViewModel(val binding: ItemViewDashboardReturnBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener {
+            }
+        }
+    }
+
+    fun initLoad(list: List<ReturnOrder>) {
+        dataList.clear()
+//        dataList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    fun pagingLoad(list: List<ReturnOrder>) {
+        val currentIndex = dataList.size
+        val newDataCount = list.size
+//        dataList.addAll(list)
+        notifyItemRangeInserted(currentIndex, newDataCount)
+    }
+
+}
--- app/src/main/java/com/bd/deliverytiger/app/ui/return_statement/details/ReturnStatementDetailsFragment.kt
+++ app/src/main/java/com/bd/deliverytiger/app/ui/return_statement/details/ReturnStatementDetailsFragment.kt
@@ -5,23 +5,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bd.deliverytiger.app.api.model.return_statement.ReturnStatementData
+import com.bd.deliverytiger.app.databinding.FragmentReturnStatementDetailsBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ReturnStatementDetailsFragment(): Fragment() {
 
+    private var binding: FragmentReturnStatementDetailsBinding? = null
+
+    private val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+    private lateinit var dataAdapter: ReturnStatementDetailsAdapter
+
+    private var returnStatementData: ReturnStatementData = ReturnStatementData()
+
     companion object {
-        fun newInstance(): ReturnStatementDetailsFragment = ReturnStatementDetailsFragment().apply {  }
+        @JvmStatic
+        fun newInstance(returnStatementData: ReturnStatementData): ReturnStatementDetailsFragment = ReturnStatementDetailsFragment().apply {
+            this.returnStatementData = returnStatementData
+        }
+
+        @JvmField
         val tag: String = ReturnStatementDetailsFragment::class.java.name
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return  FragmentReturnStatementDetailsBinding.inflate(inflater, container, false).also {
+            binding = it
+        }.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        dataAdapter = ReturnStatementDetailsAdapter()
+        dataAdapter.initLoad(returnStatementData.orders)
+        with(binding?.recyclerview!!) {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = dataAdapter
+            //addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
+        }
+
+
     }
 
+
     override fun onDestroyView() {
+        binding = null
         super.onDestroyView()
     }
 
