97b3b3f8ec052d03e79355e89733b963f07e0d5e
Anup <anupdey99@gmail.com>
2021-07-15 12:09:07 +0600

    collector arrival count down updated

--- app/src/main/java/com/bd/deliverytiger/app/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/bd/deliverytiger/app/ui/dashboard/DashboardViewModel.kt
@@ -563,16 +563,16 @@ class DashboardViewModel(private val repository: AppRepository) : ViewModel() {
                         }
                     }
                     is NetworkResponse.ServerError -> {
-                        val message = "দুঃখিত, এই মুহূর্তে আমাদের সার্ভার কানেকশনে সমস্যা হচ্ছে, কিছুক্ষণ পর আবার চেষ্টা করুন"
-                        viewState.value = ViewState.ShowMessage(message)
+                        //val message = "দুঃখিত, এই মুহূর্তে আমাদের সার্ভার কানেকশনে সমস্যা হচ্ছে, কিছুক্ষণ পর আবার চেষ্টা করুন"
+                        //viewState.value = ViewState.ShowMessage(message)
                     }
                     is NetworkResponse.NetworkError -> {
-                        val message = "দুঃখিত, এই মুহূর্তে আপনার ইন্টারনেট কানেকশনে সমস্যা হচ্ছে"
-                        viewState.value = ViewState.ShowMessage(message)
+                        //val message = "দুঃখিত, এই মুহূর্তে আপনার ইন্টারনেট কানেকশনে সমস্যা হচ্ছে"
+                        //viewState.value = ViewState.ShowMessage(message)
                     }
                     is NetworkResponse.UnknownError -> {
-                        val message = "কোথাও কোনো সমস্যা হচ্ছে, আবার চেষ্টা করুন"
-                        viewState.value = ViewState.ShowMessage(message)
+                        //val message = "কোথাও কোনো সমস্যা হচ্ছে, আবার চেষ্টা করুন"
+                        //viewState.value = ViewState.ShowMessage(message)
                         Timber.d(response.error)
                     }
                 }
